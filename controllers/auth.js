779841499af609756636da0f0a997ec5d2a156e1
Mueen <hejiy2002@gmail.com>
2025-08-13 00:52:50 +0300

    update

--- controllers/auth.js
+++ controllers/auth.js
@@ -36,9 +36,13 @@ router.post("/sign-up", async (req, res) => {
     req.body.password = hashedPassword;
 
     // All ready to create the new user!
-    await User.create(req.body);
-
-    res.redirect("/auth/sign-in");
+    const newUser = await User.create(req.body);
+   
+   req.session.user = {
+      username: newUser.username,
+      _id: newUser._id,
+    };
+    res.redirect("/");
   } catch (error) {
     console.log(error);
     res.redirect("/");
